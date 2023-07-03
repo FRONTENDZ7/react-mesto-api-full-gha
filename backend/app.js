12792724beb3d1dd0508c7d3197328202928f151
burondt <denisalexandrovich@inbox.ru>
2023-07-03 15:42:40 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,11 @@ mongoose.set('strictQuery', false);
 mongoose.connect(mongoDB);
 
 app.use(express.json());
-app.use(requestLogger);
+
 app.use(helmet());
+
+app.use(requestLogger);
 app.use(limiter);
-
-
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас остановится');
